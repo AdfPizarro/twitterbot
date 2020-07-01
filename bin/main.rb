94bc8509069addb15399c953113bafdf17c07c56
Adrian <adfpizarro@gmail.com>
2020-07-01 14:06:17 -0600

    Added method to get only the new mentions

--- bin/main.rb
+++ bin/main.rb
@@ -1,29 +1,8 @@
 require_relative '../lib/twitt.rb'
-
 twitt=Twitt.new
 
 
-#twitt.tweet('Class test')
 
-mentions =twitt.getMentions
-#File.write('tweets.yml', YAML.dump(mentions))
+
+mentions =twitt.getNewMentions
 mentions.each {|mention| puts mention::text}
-
-
-
-
-
-##client.update('@ Hello human', in_reply_to_status_id: 1278077477471096833 )
-
-
-
-
-
-#File.write('tweets.yml', YAML.dump(mentions))
-
-#tweets = client.user_timeline('rubyinside', count: 20)
-#tweets.each { |tweet| puts tweet.full_text }
-
-#
-#File.write('tweets.yml', YAML.dump(tweets))
-#    client.update("Test")
--- lib/twitt.rb
+++ lib/twitt.rb
@@ -15,13 +15,23 @@ class Twitt
   end
 
   def tweet(message)
-    p @client.update(message)
+    @client.update(message)
 
   end
 
-  def getMentions
+  def getAllMentions
     return @client.mentions_timeline
   end
+
+  def getNewMentions
+    savedMentions=YAML.load(File.read("tweets.yml"))
+    lastMentionDate=savedMentions[0]::created_at
+    allMentions = @client.mentions_timeline
+    newMentions=allMentions.select { |mention| mention::created_at > lastMentionDate }
+    File.write('tweets.yml', YAML.dump(allMentions))
+    return newMentions
+  end
+
 
   def reply(message, tweet)
     client.update( '@'+mention::user::screen_name+' '+message, in_reply_to_status_id: mention::id )
